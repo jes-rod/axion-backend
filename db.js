447e7499e052a0dfe45384166f6c5ff05a685729
ChesterK <jas.rod121@gmail.com>
2023-12-01 13:47:55 -0600

    first commit

---
+++ db.js
@@ -0,0 +1,144 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config({path: './.env.prod'});
+
+const uri = process.env.DB_URL;
+const client =  new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+const login = async (user) => {
+  try {
+    await client.connect();
+    const database = client.db("axion");
+    const users = database.collection("users");
+    const result = await users.findOne({email: user.email});
+    if (result === null){
+      await client.close();
+      return false;
+    }
+    await client.close();
+    return result.password;
+  }
+  catch(error){
+    console.log(error)
+    return error.message
+  }    
+}
+
+const register = async (user) => {
+  try {
+    await client.connect();
+    const database = client.db("axion");
+    const users = database.collection("users");
+    const result = await users.insertOne({fullName: user.fullName,  email: user.email, password: user.password, phone: user.phone, address: user.address});
+    await client.close();
+    return 'User added successfully';
+  }
+  catch(error){
+    console.log(error)
+    return error.message
+  }    
+}
+
+const update = async (user) => {
+  try {
+    await client.connect();
+    const database = client.db("axion");
+    const users = database.collection("users");
+    const result = await users.updateOne({email: user.email}, {fullName: user.fullName,  email: user.email, password: user.password, phone: user.phone, address: user.address});
+    await client.close();
+    return 'User updated successfully';
+  }
+  catch(error){
+    console.log(error)
+    return error.message
+  }    
+}
+
+const placeOrder = async (order) => {
+  try {
+    await client.connect();
+    const database = client.db("axion");
+    const orders = database.collection("orders");
+    const result = await orders.insertOne({orderID: order.id,  email: order.email, products: order.products, totalCost: order.totalCost, arrival: order.arrival});
+    await client.close();
+    return 'User added successfully';
+  }
+  catch(error){
+    console.log(error)
+    return error.message
+  }    
+}
+
+const getOrders = async (user) => {
+  try {
+    await client.connect();
+    const database = client.db("axion");
+    const orders = database.collection("orders");
+    const result = await orders.find({email: user.email});
+    await client.close();
+    return result;
+  }
+  catch(error){
+    console.log(error)
+    return error.message
+  }    
+}
+
+const getOrder = async (orderID) => {
+  try {
+    await client.connect();
+    const database = client.db("axion");
+    const orders = database.collection("orders");
+    const result = await orders.find({order: orderID});
+    await client.close();
+    return result;
+  }
+  catch(error){
+    console.log(error)
+    return error.message
+  }    
+}
+
+const getProducts = async () => {
+  try {
+    await client.connect();
+    const database = client.db("axion");
+    const products = database.collection("products");
+    const result = await products.find({}).toArray();
+    await client.close();
+    console.log(result);
+    return result;
+  }
+  catch(error){
+    console.log(error)
+    return error.message
+  }    
+}
+
+const search = async (user) => {
+  try {
+    await client.connect();
+    const database = client.db("axion");
+    const users = database.collection("users");
+    const result = await users.findOne({email: user.email});
+    if (result === null){
+      await client.close();
+      return false;
+    }
+    await client.close();
+    return result.email;
+  }
+  catch(error){
+    console.log(error)
+    return error.message
+  }    
+}
+
+
+module.exports = {login, register, search, placeOrder, getOrder, getOrders, getProducts, update}
---
+++ server.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 8000;
+const {login, register, search, getProducts, placeOrder, getOrder, getOrders, update} = require('./db.js');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const bodyParser = require('body-parser')
+require('dotenv').config({path: './.env.prod'});
+
+app.use(cors());
+app.use(bodyParser.json());
+
+/*
+const auth = async (req, res, next) => {
+  try {
+    //   get the token from the authorization header
+    const token = await req.headers.authorization.split(" ")[1];
+
+    //check if the token matches the supposed origin
+    const decodedToken = await jwt.verify(token, "RANDOM-TOKEN");
+
+    // retrieve the user details of the logged in user
+    const user = await decodedToken;
+
+    // pass the the user down to the endpoints here
+    req.user = user;
+
+    // pass down functionality to the endpoint
+    next();
+    
+  } catch (error) {
+    res.status(401).json({
+      error: "You need to login first to access this page"
+    });
+  }
+};
+*/
+app.get('/', (req, res) => {
+  res.send('Server functioning correctly');
+})
+
+app.get('/products', async (req, res) => {
+  const products = await getProducts();
+  res.send(products);
+})
+
+app.post('/orders', async (req, res) => {
+  const user = req.body;
+  const orders = await getOrders(user.email);
+  if(orders.message){
+    res.status(500).send("Error querying orders database, please try again later");
+  }else if(!orders.orderID){
+    res.status(404).send("No orders were found");
+  }else{
+    res.send(orders);
+  }
+})
+
+app.post('/order', async (req, res) => {
+  const id = req.body.id;
+  const order = await getOrders(id);
+  if(order.message){
+    res.status(500).send("Error querying orders database, please try again later");
+  }else if(!order.orderID){
+    res.status(404).send("Error querying this order");
+  }else{
+    res.send(order);
+  }
+})
+
+app.post('/login', async (req, res) => {
+  const user = req.body;
+  const password = await login(user);
+  if(!password){
+    res.status(404).send('User not found, please check that the email address entered is correct')
+    return;
+  }
+  const passwordsMatch = await bcrypt.compare(user.password, password);
+  if(!passwordsMatch){
+    res.status(400).send('Incorrect password');
+    return;
+  }else{
+    const token = jwt.sign(
+      {
+        email: user.email,
+      },
+      "RANDOM-TOKEN",
+      { expiresIn: "24h" }
+    );
+    res.status(200).send({response: "Login successful", email: user.email, token});
+  }
+  
+})
+
+app.post('/register', async (req, res) => {
+  const user = req.body;
+  const address = await search(user);
+  if(address){
+    res.status(400).send('Email address already exists');
+    return;
+  }
+  const hpassword = await bcrypt.hash(user.password, parseInt(process.env.SALT, 10))
+  user.password = hpassword;
+  const response = await register(user);
+  console.log(response);
+  res.send('User registered successfully');
+})
+
+
+app.post('/update', async (req, res) => {
+  const user = req.body;
+  const result = await update(user);
+  if(result === "User updated successfully"){
+    res.status(200).send(result);
+  }else{
+    res.status(500).send(result);
+  }
+
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+})
